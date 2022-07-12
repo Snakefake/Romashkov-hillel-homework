41a25e9ca0cdcadf51902e4ac4b10b0cda2125e5
Snakefake <40857013+Snakefake@users.noreply.github.com>
2022-07-12 14:54:58 +0300

    123
    123

--- homework-5/dz1.js
+++ homework-5/dz1.js
@@ -2,13 +2,13 @@
 let max = findMax(arr);
 
 function findMax(arr) {
+
     let max = -Infinity
     let i = 0;
-    function f2() {
         i++;
         let el = arr[i];
-        if (i >= arr.length) return;
-        if (arr[i] >= max) return max;
+        if (i >= arr.length) return arr[0];
+        if (arr[i] > max) return Math.max(...arr);
 
 
         if ( el > max )
@@ -17,16 +17,9 @@ function findMax(arr) {
             el = findMax( el );
         } 
            
-    }
-    f2();
+
     return max;
 }
 console.log(findMax([8]), 'one element test, must return 8');
 console.log(findMax([1, 8, 37, 5, 17]), '5 elements test, must return 37');
-console.log(findMax([8, 17]), '2 elements test, must return 17');
- */
-
-
-
-//for(let i = 0; i < arr.length; i++);
-//max = max >= arr ? max : arr;
+console.log(findMax([8, 17]), '2 elements test, must return 17'); */
