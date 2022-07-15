97a0bb2426bc1d334f6d812bb5f725d7ec3fd5c9
Snakefake <40857013+Snakefake@users.noreply.github.com>
2022-07-15 15:49:03 +0300

    123
    123

--- homework-5/dz4.js
+++ homework-5/dz4.js
@@ -1,7 +1,29 @@
+/* // Находит остаток от деления
+
+
 function quotient(a, b){
     return a > b ? a % b : b % a;
 }
 //quotient();
+
+function f(a, b, func){
+    return func (a, b);
+}
+
+result = f(44,5,quotient);
+console.log(result); */
+
+
+
+
+
+
+// Находит неполный остаток от деления
+
+
+function quotient(a, b){
+    return Math.trunc(Math.floor(a / b));
+}
 
 function f(a, b, func){
     return func (a, b);
