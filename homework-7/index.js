12b475f69bb16044723d92c739dc119457fe2f59
Snakefake <40857013+Snakefake@users.noreply.github.com>
2022-07-28 15:03:28 +0300

    123
    123

---
+++ homework-7/index.js
@@ -0,0 +1,13 @@
+const closePopUp = document.getElementById('pop_up__close');
+const openPopUp = document.getElementById('open_pop__up');
+const popUp = document.getElementById('pop_up');
+
+openPopUp.addEventListener('click', function(e){
+    e.preventDefault();
+    popUp.classList.add('active');   
+})
+
+closePopUp.addEventListener('click', () => {
+    popUp.classList.remove('active');
+})
+
